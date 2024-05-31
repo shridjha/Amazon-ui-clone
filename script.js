f8b7317fc1b91d64f40b4b2b725580057e47f0a4
Shrid <shridjha1@gmail.com>
2024-05-31 13:46:31 +0530

    Added sidebar

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ function handleLeftBtn() {
 slideBtnRight.addEventListener("click", handleRightBtn);
 
 function handleRightBtn() {
-    // if(startSlider > -endSlider){
-    //     startSlider = startSlider -100;
+     if(startSlider > -endSlider){
+         startSlider = startSlider -100;
         
-    // }
+    }
     
     imgItem.forEach(element =>{
         element.style.transform = `translateX(${startSlider}%)`;
@@ -43,6 +43,23 @@ function handleRightBtn() {
 
         handleRightBtn();
      }
+     //startSlider = 0;
      setInterval(autoSlide, 2000);
+
+
+     //sidebar navigation
+
+     const sidebarNavigation = document.getElementById("sidebar-container");
+     const sidebarOpenNav= document.getElementById("open-nav-sidebar");
+     const sidebarCloseNav= document.getElementById("close-btn");
+        
+     
+     sidebarOpenNav.addEventListener("click",()=>{
+            sidebarNavigation.classList.toggle("sidebar-show");
+        });
+
+     sidebarCloseNav.addEventListener("click",()=>{ 
+        sidebarNavigation.classList.toggle("sidebar-show");});  
+
 
     
